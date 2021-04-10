b90ce2e65db23b87e6e3b819e91af81dabede83d
Stefan Dragicevic <stefan.dragicevic0@gmail.com>
2021-04-10 22:36:23 +0200

    Add movie listings and reposivness fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,32 @@
 <template>
-  <div class="home">
-    <Mostpopular/>
+  <div class="upcoming">
+    <h2 class="mb-30">Most popular</h2>
+
+    <div class="view__grid-wrapper">
+      <div v-for="mostPopular in popular" :key="mostPopular.id">
+        <a href="">
+          <img :src="`https://image.tmdb.org/t/p/w500` + mostPopular.poster_path" alt="" srcset="">
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Mostpopular from '@/components/Mostpopular.vue'
+import axios from 'axios'
 
 export default {
-  name: 'Home',
-  components: {
-    Mostpopular
+  name: 'Mostpopular',
+  data () {
+    return {
+      popular: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.themoviedb.org/3/movie/popular?api_key=6fa7ab7aeacd59b453d8dbb3b3d65234&language=en-US&page=1')
+      .then(response => (this.popular = response.data.results))
+      .catch(error => console.log(error))
   }
 }
 </script>
