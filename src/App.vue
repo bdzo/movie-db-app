1f0719320c9755a703b0b1c3bada7befb3fdaefe
Stefan Dragicevic <stefan.dragicevic0@gmail.com>
2021-04-24 23:08:23 +0200

    Add search input functionality

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,12 @@
 <template>
   <div id="app">
-
-    <header>
-      <div class="header__logo">
-        <router-link to="/">MovieDB</router-link>
-      </div>
-
-      <div class="header__search-bar">
-        <input type="text" name="" id="" placeholder="Search for the movie...">
-      </div>
-      
-      <nav class="header__nav"> 
-        <router-link to="/toprated">Top rated</router-link>
-        <router-link to="/upcoming">Upcoming</router-link>
-        <router-link to="/favorites">Favorites</router-link>
-      </nav>
-    </header>
+    <Header></Header>
 
     <div class="view__main-wrapper">
       <router-view/>
     </div>
 
-    <footer>
-      <p>2021. Copyright. All rights reserved. MoviesDB</p>
-    </footer>
+    <Footer></Footer>
   </div>
 </template>
 
@@ -31,3 +14,15 @@
 @import 'assets/css/style.css';
 @import 'assets/css/responsive.css';
 </style>
+
+<script>
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export default {
+ components: {
+   Header,
+   Footer
+ } 
+}
+</script>
